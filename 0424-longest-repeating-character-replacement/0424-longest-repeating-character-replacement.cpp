08706a70b802d13a7ae51efb6b71394314def31c
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-01-16 18:20:40 +0530

    Time: 0 ms (100%), Space: 10.7 MB (91.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,26 +3,26 @@ public:
     int characterReplacement(string s, int k) {
         vector<int> freq(26, 0);
 
-        int i = 0;          // start pointer
-        int replace = 0;    // replacement count
-        int maxFreq = 0;    // most frequent char count
+        int i = 0;        
+        int replace = 0;  
+        int maxFreq = 0;   
         int ans = 0;
 
         for (int j = 0; j < s.size(); j++) {
-            // include current character
+            
             freq[s[j] - 'A']++;
             maxFreq = max(maxFreq, freq[s[j] - 'A']);
 
-            // replacements needed
+            
             replace = (j - i + 1) - maxFreq;
 
-            // if replacements exceed k, move start
+            
             if (replace > k) {
                 freq[s[i] - 'A']--;
                 i++;
             }
 
-            // valid window
+            
             ans = max(ans, j - i + 1);
         }
 
