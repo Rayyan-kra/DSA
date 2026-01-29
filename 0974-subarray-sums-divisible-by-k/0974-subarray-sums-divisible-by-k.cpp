f8a3f890e2a70bbd136f8448bc245d237005e707
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-01-30 01:29:50 +0530

    Time: 6 ms (81.91%), Space: 35.4 MB (88.68%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         unordered_map<int,int> freq;
-        freq[0] = 1;       
+        freq[0] = 1;      
         int curr = 0;
         int ans = 0;
 
         for(int x : nums) {
             curr += x;
-            int rem = ((curr % k) + k) % k;   
-
-            ans += freq[rem];               
-            freq[rem]++;                       
+            int rem = ((curr % k) + k) % k;  
+            if(freq.count(rem)) {
+                ans += freq[rem];            
+            }
+            freq[rem]++;                     
         }
 
         return ans;
