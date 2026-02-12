e06444c2f9044694399e94214a867be969eef931
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-02-12 18:28:13 +0530

    Time: 0 ms (100%), Space: 31.2 MB (93.85%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int leftCount = countNodes(root->left);
+        int rightCount = countNodes(root->right);
+
+        return 1 + leftCount + rightCount;
+    }
+};
