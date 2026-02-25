50a97224fdb3844e2ac136f7cac68c0461cc9d73
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-02-25 23:37:45 +0530

    Time: 0 ms (100%), Space: 13.5 MB (23.49%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+     int helper(vector<vector<int>> &triangle, vector<vector<int>> &dp,int row,int col){
+        int n= triangle.size();
+        if(row==n-1) return triangle[row][col];
+        if(dp[row][col]!=INT_MAX) return dp[row][col];
+        int left= helper(triangle,dp,row+1,col);
+        int right=helper(triangle,dp,row+1,col+1);
+        return dp[row][col]=min(left,right)+triangle[row][col];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        
-        vector<int> dp = triangle[n - 1];  // start from last row
-        
-        for(int i = n - 2; i >= 0; i--) {
-            for(int j = 0; j <= i; j++) {
-                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
-            }
-        }
-        
-        return dp[0];
+        int n= triangle.size();
+        vector<vector<int>> dp(n,vector<int>(triangle.size(),INT_MAX));
+       return helper(triangle,dp,0,0); 
     }
 };
