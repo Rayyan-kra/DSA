0e3af229156eaad910b650d3e49af1138ee4a2d6
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-04-08 00:00:58 +0530

    Time: 39 ms (13.6%), Space: 42.7 MB (11.31%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void findcombine(int index,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds){
-        if(index == arr.size()){
-            if(target == 0){
-                ans.push_back(ds);
-            }
+    void helper(int idx, vector<int>& candidates, int target,
+                vector<vector<int>> &ans, vector<int> ds) {
+
+        if (target == 0) {
+            ans.push_back(ds);
             return;
         }
-        if(arr[index]<=target){
-            ds.push_back(arr[index]);
-            findcombine(index,target-arr[index],arr,ans,ds);
+
+        if (idx >= candidates.size()) return;  
+
+        if (candidates[idx] <= target) {      
+            ds.push_back(candidates[idx]);
+            helper(idx, candidates, target - candidates[idx], ans, ds);
             ds.pop_back();
         }
-        findcombine(index+1,target,arr,ans,ds);
+
+        helper(idx + 1, candidates, target, ans, ds);
     }
-    vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
-      vector<vector<int>> ans;
-      vector<int> ds;
-      findcombine(0,target,candidates,ans,ds);
-      return ans;
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        helper(0, candidates, target, ans, ds);
+        return ans;
     }
 };
