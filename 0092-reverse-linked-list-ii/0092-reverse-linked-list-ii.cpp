1b92cd9c564760ce3ce3995fa7b791fa7505d9a0
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-04-19 22:38:01 +0530

    Time: 0 ms (100%), Space: 11.3 MB (38.03%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -6,18 +6,16 @@ public:
         ListNode dummy(0);
         dummy.next = head;
         ListNode* prev = &dummy;
-
-        // move to (left-1) position
         for (int i = 1; i < left; i++) {
             prev = prev->next;
         }
 
         ListNode* curr = prev->next;
-        ListNode* tail = curr;   // will become end after reverse
+        ListNode* tail = curr;  
         ListNode* next = NULL;
         ListNode* pre = NULL;
 
-        // reverse only required part
+        
         for (int i = 0; i <= right - left; i++) {
             next = curr->next;
             curr->next = pre;
@@ -25,7 +23,7 @@ public:
             curr = next;
         }
 
-        // reconnect
+        
         prev->next = pre;
         tail->next = curr;
 
