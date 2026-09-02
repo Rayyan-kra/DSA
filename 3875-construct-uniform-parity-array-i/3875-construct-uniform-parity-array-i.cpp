7dd8fa1d22f080a1cd573a1c642863ea866267a3
Rayyan Chaman Ansari <rayyanchaman777@gmail.com>
2026-09-02 23:05:42 +0530

    Time: 0 ms (100%), Space: 30.6 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-      return true;  
+    int n=nums1.size()-1;
+    vector<int> nums2;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(nums1[i]%2==0){
+            nums2.push_back(nums1[i]);
+        }
+        else{
+            sum=nums1[i]-nums1[i+1];
+            nums2.push_back(sum);
+        }
+    }    
+    for(int j=0;j<n;j++){
+        if(nums2[j]%2==0){
+            return true;
+        }
+        else{
+            return true;
+        }
+    }
+    return true;
     }
 };
